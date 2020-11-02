6c0d3e447cc098776986fcd5cb39816bec70cf35
Dongmin <ironluffy95@gmail.com>
2020-11-02 20:40:21 +0900

    aug_train bug fixed (load augmentations)

--- FastAutoAugment/aug_eval/aug_train.py
+++ FastAutoAugment/aug_eval/aug_train.py
@@ -45,18 +45,21 @@ def train(args, logger):
     num_points = 1024
 
     if os.path.isfile(
-            'aug_final/{}2{}_op{}_ncv{}_npy{}_ns{}.pth'.format(args.source_domain, args.target_domain, args.num_op,
-                                                               args.num_cv,
-                                                               args.num_policy, args.num_search)):
+            'aug_final/{}_{}2{}_op{}_ncv{}_npy{}_ns{}.pth'.format(args.dc_model, args.source_domain, args.target_domain,
+                                                                  args.num_op,
+                                                                  args.num_cv,
+                                                                  args.num_policy, args.num_search)):
         aug_load = torch.load(
-            'aug_final/{}2{}_op{}_ncv{}_npy{}_ns{}.pth'.format(args.source_domain, args.target_domain, args.num_op,
-                                                               args.num_cv,
-                                                               args.num_policy, args.num_search))
+            'aug_final/{}_{}2{}_op{}_ncv{}_npy{}_ns{}.pth'.format(args.dc_model, args.source_domain, args.target_domain,
+                                                                  args.num_op,
+                                                                  args.num_cv,
+                                                                  args.num_policy, args.num_search))
     else:
         aug_load = torch.load(
-            'aug_final/{}2{}_op{}_ncv{}_npy{}.pth'.format(args.source_domain, args.target_domain, args.num_op,
-                                                          args.num_cv,
-                                                          args.num_policy))
+            'aug_final/{}_{}2{}_op{}_ncv{}_npy{}.pth'.format(args.dc_model, args.source_domain, args.target_domain,
+                                                             args.num_op,
+                                                             args.num_cv,
+                                                             args.num_policy))
 
     aug = Augmentation(aug_load)
 
