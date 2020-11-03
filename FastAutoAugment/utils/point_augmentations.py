15b87792739bc222f1d31da7f43b488240b924b3
Dongmin <ironluffy95@gmail.com>
2020-11-03 14:48:13 +0900

    transformations update

--- FastAutoAugment/utils/point_augmentations.py
+++ FastAutoAugment/utils/point_augmentations.py
@@ -4,15 +4,16 @@ import utils.point_cloud_utils as pcu
 from utils.emd.emd_module import emdModule
 
 
-def random_crop_plane(point_clouds, min_rad=0.6, occlude_range=0.3):
+def random_crop_plane(point_clouds, level=0.6, random_range=0.3):
     B, C, N = point_clouds.shape
     device = point_clouds.device
-    min_rad = min_rad * (1 - occlude_range)
+    level = 1.0 - level
+    min_rad = level * (1 - random_range)
     cropped_point_clouds = torch.zeros([0], dtype=torch.float32, device=device)
 
     for idx in range(B):
         point_cloud = point_clouds[idx, :].unsqueeze(0)
-        rad = np.random.uniform(min_rad, min_rad + occlude_range)
+        rad = np.random.uniform(min_rad, min_rad + random_range)
         dir_vec = torch.rand([1, 3, 1]).to(device)
         pos_vec = (dir_vec / pcu.get_distance(dir_vec).unsqueeze(dim=2) * rad)
         threshold_val = (point_cloud - pos_vec).permute(0, 2, 1).bmm(dir_vec)
@@ -33,10 +34,10 @@ def random_crop_plane(point_clouds, min_rad=0.6, occlude_range=0.3):
     return cropped_point_clouds
 
 
-def random_crop_sphere(point_clouds, min_rad=0.6, occlude_range=0.3):
+def random_crop_sphere(point_clouds, level=0.6, random_range=0.3):
     B, C, N = point_clouds.shape
     device = point_clouds.device
-    min_rad = min_rad * (0.8 - occlude_range)
+    min_rad = level * (0.8 - random_range)
     cropped_point_clouds = torch.zeros([0], dtype=torch.float32, device=device)
 
     for idx in range(B):
@@ -53,7 +54,7 @@ def random_crop_sphere(point_clouds, min_rad=0.6, occlude_range=0.3):
             distance = pcu.get_distance(point_cloud.sub(view_point))
 
             temp_point_cloud = point_cloud.transpose(2, 1)
-            max_rad = min_rad + occlude_range
+            max_rad = min_rad + random_range
             mask_f = (distance > np.random.uniform(min_rad, max_rad))
 
             temp_point_cloud = temp_point_cloud[mask_f, :].unsqueeze(0).transpose(2, 1)
@@ -73,10 +74,11 @@ def random_crop_sphere(point_clouds, min_rad=0.6, occlude_range=0.3):
     return cropped_point_clouds
 
 
-def random_crop_sphere_reverse(point_clouds, min_rad=0.6, occlude_range=0.3):
+def random_crop_sphere_reverse(point_clouds, level=0.6, random_range=0.3):
     B, C, N = point_clouds.shape
     device = point_clouds.device
-    min_rad = min_rad * (1 - (occlude_range + 0.1)) + 0.1
+    level = 1.0 - level
+    min_rad = level * (1 - (random_range + 0.1)) + 0.1
     cropped_point_clouds = torch.zeros([0], dtype=torch.float32, device=device)
 
     for idx in range(B):
@@ -93,7 +95,7 @@ def random_crop_sphere_reverse(point_clouds, min_rad=0.6, occlude_range=0.3):
             distance = pcu.get_distance(point_cloud.sub(view_point))
 
             temp_point_cloud = point_cloud.transpose(2, 1)
-            max_rad = min_rad + occlude_range
+            max_rad = min_rad + random_range
             mask_f = (distance < np.random.uniform(min_rad, max_rad))
 
             temp_point_cloud = temp_point_cloud[mask_f, :].unsqueeze(0).transpose(2, 1)
@@ -113,14 +115,14 @@ def random_crop_sphere_reverse(point_clouds, min_rad=0.6, occlude_range=0.3):
     return cropped_point_clouds
 
 
-def naive_point_mix_up(point_clouds, mixup_param=1.0, random_range=0.3):
+def naive_point_mix_up(point_clouds, level=1.0, random_range=0.3):
     B, _, N = point_clouds.shape
     device = point_clouds.device
 
     shuffled_index = torch.randperm(B).to(device)
     shuffled_pc = point_clouds[shuffled_index, :]
 
-    lam = (1 - random_range) * mixup_param + torch.FloatTensor(B).uniform_(0, random_range)
+    lam = (1 - random_range) * level + torch.FloatTensor(B).uniform_(0, random_range)
     lam = lam.to(device)
 
     num_pts_a = torch.round(lam * N)
@@ -150,7 +152,7 @@ def naive_point_mix_up(point_clouds, mixup_param=1.0, random_range=0.3):
     return mixed_point_clouds  # , (label_a, label_b, lam)
 
 
-def point_mix_up(point_clouds, mixup_param=1.0, random_range=0.3):
+def point_mix_up(point_clouds, level=1.0, random_range=0.3):
     emd_loss2 = emdModule()
     B, _, N = point_clouds.shape
     device = point_clouds.device
@@ -158,7 +160,7 @@ def point_mix_up(point_clouds, mixup_param=1.0, random_range=0.3):
     shuffled_index = torch.randperm(B).to(device)
     shuffled_point_clouds = point_clouds[shuffled_index, :].to(device)
 
-    lam = (1 - random_range) * mixup_param + torch.FloatTensor(B).uniform_(0, random_range)
+    lam = (1 - random_range) * level + torch.FloatTensor(B).uniform_(0, random_range)
     lam = lam.to(device)
     emd_loss, emd_matching_idx = emd_loss2(point_clouds.permute(0, 2, 1),
                                            shuffled_point_clouds.permute(0, 2, 1), 0.05, 3000)
@@ -176,42 +178,65 @@ def point_mix_up(point_clouds, mixup_param=1.0, random_range=0.3):
     return mixup_point_cloud  # , (label_a, label_b, lam)
 
 
-def squeeze_z(point_cloud, min_squeeze_param=0.6, random_range=0.3):
-    B, C, N = point_cloud.shape
-    device = point_cloud.device
-    min_squeeze_param = min_squeeze_param * (1 - random_range)
+def point_mix_up_random_access(point_clouds, level=1.0, random_range=0.3):
+    B, _, N = point_clouds.shape
+    device = point_clouds.device
+
+    shuffled_index = torch.randperm(B).to(device)
+    shuffled_point_clouds = point_clouds[shuffled_index, :].to(device)
+
+    lam = (1 - random_range) * level + torch.FloatTensor(B).uniform_(0, random_range)
+    lam = lam.to(device)
+
+    mixup_point_cloud = shuffled_point_clouds * lam.view(-1, 1, 1) + point_clouds * (1 - lam).view(-1, 1, 1)
+
+    # label_a = labels.clone()
+    # label_b = labels[shuffled_index].clone()
+
+    mixup_point_cloud = pcu.normalize(mixup_point_cloud)
+
+    return mixup_point_cloud  # , (label_a, label_b, lam)
+
+
+def squeeze_z(point_clouds, level=0.6, random_range=0.3):
+    B, C, N = point_clouds.shape
+    device = point_clouds.device
+    level = 1.0 - level
+    min_squeeze_param = level * (1 - random_range)
     squeeze_param = torch.FloatTensor(B, 1).uniform_(min_squeeze_param, min_squeeze_param + random_range)
 
-    scale_vector = torch.cat([torch.ones(point_cloud.size(0), 1),
-                              torch.ones(point_cloud.size(0), 1), squeeze_param], dim=1).to(device)
+    scale_vector = torch.cat([torch.ones(point_clouds.size(0), 1),
+                              torch.ones(point_clouds.size(0), 1), squeeze_param], dim=1).to(device)
 
-    squeeze_point_cloud = point_cloud * scale_vector.unsqueeze(-1)
+    squeeze_point_cloud = point_clouds * scale_vector.unsqueeze(-1)
 
     squeeze_point_cloud = pcu.normalize(squeeze_point_cloud)
 
     return squeeze_point_cloud
 
 
-def squeeze_xy(point_cloud, min_squeeze_param=0.6, random_range=0.3):
-    B, C, N = point_cloud.shape
-    device = point_cloud.device
-    min_squeeze_param = min_squeeze_param * (1 - random_range)
+def squeeze_xy(point_clouds, level=0.6, random_range=0.3):
+    B, C, N = point_clouds.shape
+    device = point_clouds.device
+    level = 1.0 - level
+    min_squeeze_param = level * (1 - random_range)
     squeeze_param = torch.FloatTensor(B, 1).uniform_(min_squeeze_param, min_squeeze_param + random_range)
     random_theta = torch.FloatTensor(B, 1).uniform_(0, 2 * np.pi)
     scale_vector = torch.cat([torch.cos(random_theta) * squeeze_param,
                               torch.sin(random_theta) * squeeze_param,
                               torch.ones(B, 1)], dim=1).to(device)
 
-    squeeze_point_cloud = point_cloud * scale_vector.unsqueeze(-1)
+    squeeze_point_cloud = point_clouds * scale_vector.unsqueeze(-1)
     squeeze_point_cloud = pcu.normalize(squeeze_point_cloud)
 
     return squeeze_point_cloud
 
 
-def squeeze_sphere(point_clouds, min_squeeze_param=0.6, random_range=0.3):
+def squeeze_sphere(point_clouds, level=0.6, random_range=0.3):
     B, C, N = point_clouds.shape
     device = point_clouds.device
-    min_squeeze_param = min_squeeze_param * (1 - random_range)
+    level = 1.0 - level
+    min_squeeze_param = level * (1 - random_range)
     squeeze_ratio = torch.FloatTensor(B, 1, 1).uniform_(min_squeeze_param, min_squeeze_param + random_range).to(device)
     viewpoint_rad = torch.FloatTensor(B, 1, 1).uniform_().to(device) * (1 - squeeze_ratio)
 
@@ -235,24 +260,30 @@ def sparse(point_clouds, level=0.6, random_range=0.3):
     device = point_clouds.device
     min_rad = level * (1 - random_range)
     augmented_point_clouds = torch.zeros([0], dtype=torch.float32, device=device)
+
     for idx in range(B):
         point_cloud = point_clouds[idx, :].unsqueeze(0)
         random_index = np.random.randint(N)
         view_point = point_cloud[:, :, random_index].unsqueeze(2)
         distance = pcu.get_distance(point_cloud.sub(view_point))
+
         max_rad = min_rad + random_range
         mask_f = (distance < np.random.uniform(min_rad, max_rad))
+
         part_point_cloud = point_cloud.transpose(2, 1)
         part_point_cloud = part_point_cloud[mask_f, :].unsqueeze(0).transpose(2, 1)
         part_point_cloud = pcu.jitter(part_point_cloud, sigma=0.02, clip=0.05)
-        if not (N - part_point_cloud.size(2) == 0):
-            point_cloud, _ = pcu.random_point_sample(point_cloud, num_points=N - part_point_cloud.size(2))
+
+        if part_point_cloud.size(2) == N:
+            augmented_point_clouds = torch.cat([augmented_point_clouds, part_point_cloud], dim=0)
+        else:
+            point_cloud, _ = pcu.random_point_sample(point_cloud, num_points=N-part_point_cloud.size(2))
             point_cloud = torch.cat([point_cloud, part_point_cloud], dim=2)
             point_cloud = pcu.point_permutate(point_cloud)
             augmented_point_clouds = torch.cat([augmented_point_clouds, point_cloud], dim=0)
-        else:
-            augmented_point_clouds = torch.cat([augmented_point_clouds, part_point_cloud], dim=0)
+
     augmented_point_clouds = pcu.normalize(augmented_point_clouds)
+
     return augmented_point_clouds
 
 
@@ -271,16 +302,16 @@ def mix_up_loss(pred, mixup_label, criterion):
 
 def augment_list(for_autoaug=True):  # 16 oeprations and their ranges
     l = [
-        (random_crop_plane, 0, 1),
-        (random_crop_sphere, 0, 1),
-        (random_crop_sphere_reverse, 0, 1),
+        (random_crop_plane, 0, 0.25),
+        (random_crop_sphere, 0, 0.25),
+        (random_crop_sphere_reverse, 0, 0.25),
         (naive_point_mix_up, 0, 0.5),
         (point_mix_up, 0, 0.5),
-        (squeeze_xy, 0, 1),
-        (squeeze_z, 0, 1),
-        (squeeze_sphere, 0, 1),
+        (squeeze_xy, 0, 0.25),
+        (squeeze_z, 0, 0.25),
+        (squeeze_sphere, 0, 0.25),
         (equalize, 0, 1),
-        (sparse, 0, 1)
+        (sparse, 0, 0.25)
     ]
     if for_autoaug:
         l += [
