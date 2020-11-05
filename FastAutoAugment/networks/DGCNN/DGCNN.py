7b1e02e84a69a9c0e591ab9bb69c56b6e6aa752a
Dongmin <ironluffy95@gmail.com>
2020-11-06 01:44:27 +0900

    aug_all update

--- FastAutoAugment/networks/DGCNN/DGCNN.py
+++ FastAutoAugment/networks/DGCNN/DGCNN.py
@@ -15,14 +15,14 @@ def knn(x, k):
     return idx
 
 
-def get_graph_feature(x, args, k=20, idx=None):
+def get_graph_feature(x, k=20, idx=None):
     batch_size = x.size(0)
     num_points = x.size(2)
     x = x.view(batch_size, -1, num_points)
     if idx is None:
         idx = knn(x, k=k)  # (batch_size, num_points, k)
     # Run on cpu or gpu
-    device = torch.device("cuda:" + str(x.get_device()) if args.cuda else "cpu")
+    device = x.device
 
     idx_base = torch.arange(0, batch_size, device=device).view(-1, 1, 1) * num_points
 
@@ -91,13 +91,12 @@ class transform_net(nn.Module):
     """ Input (XYZ) Transform Net, input is BxNx3 gray image
         Return: Transformation matrix of size 3xK """
 
-    def __init__(self, args, in_ch, out=3):
+    def __init__(self, in_ch, out=3):
         super(transform_net, self).__init__()
         self.K = out
-        self.args = args
 
-        activation = 'leakyrelu' if args.model == 'dgcnn' else 'relu'
-        bias = False if args.model == 'dgcnn' else True
+        activation = 'leakyrelu'
+        bias = False
 
         self.conv2d1 = conv_2d(in_ch, 64, kernel=1, activation=activation, bias=bias)
         self.conv2d2 = conv_2d(64, 128, kernel=1, activation=activation, bias=bias)
@@ -107,13 +106,13 @@ class transform_net(nn.Module):
         self.fc3 = nn.Linear(256, out * out)
 
     def forward(self, x):
-        device = torch.device("cuda:" + str(x.get_device()) if self.args.cuda else "cpu")
+        device = x.device
 
         x = self.conv2d1(x)
         x = self.conv2d2(x)
-        if self.args.model == "dgcnn":
-            x = x.max(dim=-1, keepdim=False)[0]
-            x = torch.unsqueeze(x, dim=3)
+
+        x = x.max(dim=-1, keepdim=False)[0]
+        x = torch.unsqueeze(x, dim=3)
         x = self.conv2d3(x)
         x, _ = torch.max(x, dim=2, keepdim=False)
         x = x.view(x.size(0), -1)
@@ -128,68 +127,12 @@ class transform_net(nn.Module):
         return x
 
 
-class PointNet(nn.Module):
-    def __init__(self, args, num_class=10):
-        super(PointNet, self).__init__()
-        self.args = args
-
-        self.trans_net1 = transform_net(args, 3, 3)
-        self.trans_net2 = transform_net(args, 64, 64)
-        self.conv1 = conv_2d(3, 64, 1)
-        self.conv2 = conv_2d(64, 64, 1)
-        self.conv3 = conv_2d(64, 64, 1)
-        self.conv4 = conv_2d(64, 128, 1)
-        self.conv5 = conv_2d(128, 1024, 1)
-
-        num_f_prev = 64 + 64 + 64 + 128
-
-        self.C = classifier(args, num_class)
-        self.DefRec = RegionReconstruction(args, num_f_prev + 1024)
-
-    def forward(self, x, activate_DefRec=False):
-        num_points = x.size(2)
-        x = torch.unsqueeze(x, dim=3)
-
-        logits = {}
-
-        transform = self.trans_net1(x)
-        x = x.transpose(2, 1)
-        x = x.squeeze(dim=3)
-        x = torch.bmm(x, transform)
-        x = x.unsqueeze(3)
-        x = x.transpose(2, 1)
-        x1 = self.conv1(x)
-        x2 = self.conv2(x1)
-        transform = self.trans_net2(x2)
-        x = x2.transpose(2, 1)
-        x = x.squeeze(dim=3)
-        x = torch.bmm(x, transform)
-        x = x.unsqueeze(3)
-        x = x.transpose(2, 1)
-        x3 = self.conv3(x)
-        x4 = self.conv4(x3)
-        x_cat = torch.cat((x1, x2, x3, x4), dim=1)
-
-        x = self.conv5(x4)
-        x5, _ = torch.max(x, dim=2, keepdim=False)
-        x = x5.squeeze(dim=2)  # batchsize*1024
-
-        logits["cls"] = self.C(x)
-
-        if activate_DefRec:
-            DefRec_input = torch.cat((x_cat.squeeze(dim=3), x5.repeat(1, 1, num_points)), dim=1)
-            logits["DefRec"] = self.DefRec(DefRec_input)
-
-        return logits
-
-
 class DGCNN(nn.Module):
-    def __init__(self, args, num_class=10):
+    def __init__(self, num_class=10):
         super(DGCNN, self).__init__()
-        self.args = args
         self.k = K
 
-        self.input_transform_net = transform_net(args, 6, 3)
+        self.input_transform_net = transform_net(6, 3)
 
         self.conv1 = conv_2d(6, 64, kernel=1, bias=False, activation='leakyrelu')
         self.conv2 = conv_2d(64 * 2, 64, kernel=1, bias=False, activation='leakyrelu')
@@ -200,8 +143,7 @@ class DGCNN(nn.Module):
         self.bn5 = nn.BatchNorm1d(1024)
         self.conv5 = nn.Conv1d(num_f_prev, 1024, kernel_size=1, bias=False)
 
-        self.C = classifier(args, num_class)
-        self.DefRec = RegionReconstruction(args, num_f_prev + 1024)
+        self.C = classifier(num_class)
 
     def forward(self, x, activate_DefRec=False):
         batch_size = x.size(0)
@@ -210,14 +152,14 @@ class DGCNN(nn.Module):
 
         # returns a tensor of (batch_size, 6, #points, #neighboors)
         # interpretation: each point is represented by 20 vectors, each of size 6
-        x0 = get_graph_feature(x, self.args, k=self.k)
+        x0 = get_graph_feature(x, k=self.k)
         # align to a canonical space (e.g., apply rotation such that all inputs will have the same rotation)
         transformd_x0 = self.input_transform_net(x0)
         x = torch.matmul(transformd_x0, x)
 
         # returns a tensor of (batch_size, 6, #points, #neighboors)
         # interpretation: each point is represented by 20 NN, each of size 6
-        x = get_graph_feature(x, self.args, k=self.k)
+        x = get_graph_feature(x, k=self.k)
         # process point and inflate it from 6 to e.g., 64
         x = self.conv1(x)
         # per each feature (from e.g., 64) take the max value from the representative vectors
@@ -225,15 +167,15 @@ class DGCNN(nn.Module):
         # returns a tensor of size e.g., (batch_size, 64, #points)
         x1 = x.max(dim=-1, keepdim=False)[0]
 
-        x = get_graph_feature(x1, self.args, k=self.k)
+        x = get_graph_feature(x1, k=self.k)
         x = self.conv2(x)
         x2 = x.max(dim=-1, keepdim=False)[0]
 
-        x = get_graph_feature(x2, self.args, k=self.k)
+        x = get_graph_feature(x2, k=self.k)
         x = self.conv3(x)
         x3 = x.max(dim=-1, keepdim=False)[0]
 
-        x = get_graph_feature(x3, self.args, k=self.k)
+        x = get_graph_feature(x3, k=self.k)
         x = self.conv4(x)
         x4 = x.max(dim=-1, keepdim=False)[0]
 
@@ -245,26 +187,22 @@ class DGCNN(nn.Module):
         x5 = F.adaptive_max_pool1d(x5, 1).view(batch_size, -1)
         x = x5
 
-        logits["cls"] = self.C(x)
+        output = self.C(x)
 
-        if activate_DefRec:
-            DefRec_input = torch.cat((x_cat, x5.unsqueeze(2).repeat(1, 1, num_points)), dim=1)
-            logits["DefRec"] = self.DefRec(DefRec_input)
-
-        return logits
+        return output
 
 
 class classifier(nn.Module):
-    def __init__(self, args, num_class=10):
+    def __init__(self, num_class=10):
         super(classifier, self).__init__()
 
-        activate = 'leakyrelu' if args.model == 'dgcnn' else 'relu'
-        bias = True if args.model == 'dgcnn' else False
+        activate = 'leakyrelu'
+        bias = True
 
         self.mlp1 = fc_layer(1024, 512, bias=bias, activation=activate, bn=True)
-        self.dp1 = nn.Dropout(p=args.dropout)
+        self.dp1 = nn.Dropout(p=0.5)
         self.mlp2 = fc_layer(512, 256, bias=True, activation=activate, bn=True)
-        self.dp2 = nn.Dropout(p=args.dropout)
+        self.dp2 = nn.Dropout(p=0.5)
         self.mlp3 = nn.Linear(256, num_class)
 
     def forward(self, x):
@@ -272,35 +210,3 @@ class classifier(nn.Module):
         x2 = self.dp2(self.mlp2(x))
         logits = self.mlp3(x2)
         return logits
-
-
-class RegionReconstruction(nn.Module):
-    """
-    Region Reconstruction Network - Reconstruction of a deformed region.
-    For more details see https://arxiv.org/pdf/2003.12641.pdf
-    """
-
-    def __init__(self, args, input_size):
-        super(RegionReconstruction, self).__init__()
-        self.args = args
-        self.of1 = 256
-        self.of2 = 256
-        self.of3 = 128
-
-        self.bn1 = nn.BatchNorm1d(self.of1)
-        self.bn2 = nn.BatchNorm1d(self.of2)
-        self.bn3 = nn.BatchNorm1d(self.of3)
-        self.dp1 = nn.Dropout(p=args.dropout)
-        self.dp2 = nn.Dropout(p=args.dropout)
-
-        self.conv1 = nn.Conv1d(input_size, self.of1, kernel_size=1, bias=False)
-        self.conv2 = nn.Conv1d(self.of1, self.of2, kernel_size=1, bias=False)
-        self.conv3 = nn.Conv1d(self.of2, self.of3, kernel_size=1, bias=False)
-        self.conv4 = nn.Conv1d(self.of3, 3, kernel_size=1, bias=False)
-
-    def forward(self, x):
-        x = self.dp1(F.relu(self.bn1(self.conv1(x))))
-        x = self.dp2(F.relu(self.bn2(self.conv2(x))))
-        x = F.relu(self.bn3(self.conv3(x)))
-        x = self.conv4(x)
-        return x.permute(0, 2, 1)
